6d0c08aeed5cc62aa319b6bb8a4353ad57440fc0
Akash Srivastava <akash.brain@gmail.com>
2016-11-23 11:28:50 +0000

    first commit

---
+++ nips2scipy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "start demo"
+for corpus in nips
+do
+    echo "downloading UCI $corpus corpus"
+    wget http://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/vocab.$corpus.txt
+    wget http://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/docword.$corpus.txt.gz
+    gunzip docword.$corpus.txt.gz
+    echo "preprocessing, translate from docword.txt to scipy format"
+    python uci_to_scipy.py docword.$corpus.txt M_$corpus.full_docs.mat
+    echo "preprocessing: removing rare words and stopwords"
+    python truncate_vocabulary.py M_$corpus.full_docs.mat vocab.$corpus.txt 100
+    
+done
